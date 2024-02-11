706649b5b6c294915812537811c7d13e6335c54a
Endruman <hayllihuk@gmail.com>
2024-02-11 11:59:23 +0300

    Solved part of task number 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
 ﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+// char[,] array = new char[,] { { 'a', 'b', '2', 'd', 'e' }, { '!', 'g', 'h', '1', 'j' } };
 
+// string GetStringFromCharArray(char[,] array)
+// {
+//     string result = string.Empty;
+//     foreach (char symbol in array)
+//     {
+//         result += symbol; // result = result + symbol
+//     }
+//     return result;
+// }
+// Console.WriteLine(GetStringFromCharArray(array));
 
 
 
 // Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+
+
 
 // Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
 
