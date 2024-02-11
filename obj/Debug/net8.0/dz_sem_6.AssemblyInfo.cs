0f3bb9398096d90edd4ecc77645ab7dc4a405c2f
Endruman <hayllihuk@gmail.com>
2024-02-11 13:01:35 +0300

    Solved part of task number 2

--- obj/Debug/net8.0/dz_sem_6.AssemblyInfo.cs
+++ obj/Debug/net8.0/dz_sem_6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dz_sem_6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6a70e33dc45e861d39f23917ea416a78a14a1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("dz_sem_6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dz_sem_6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
